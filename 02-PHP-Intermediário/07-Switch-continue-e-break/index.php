abe1a66893c6212fd605dff50f671644fffcfdac
diegocrivelaro <contato.diegocrivelaro@gmail.com>
2021-08-16 07:29:04 -0300

    Poste um pedaço do exercício de Switch de PHP

---
+++ 02-PHP-Intermediário/07-Switch-continue-e-break/index.php
@@ -0,0 +1,12 @@
+<?php
+    $nome = "Diego";
+    switch($nome){
+        case "Diego":
+            echo "Diego";
+            break;
+        case "João":
+            echo "João";
+            break;
+    }
+
+?>
