ae9f68e65ce716ab70050a483cc26fc447d9b119
diegocrivelaro <contato.diegocrivelaro@gmail.com>
2021-08-23 18:30:45 -0300

    feat(component) Adicione a aula 10
    Foi adicionado a aula 10 que contém o conteúdo sobre sessões e cookies no php

---
+++ 02-PHP-Intermediário/10-Sessoes-e-Cookies/index.php
@@ -0,0 +1,17 @@
+<?php
+session_start(); /* INICIAR A SESSÃO */
+
+$_SESSION["Chave"] = "Valor";
+$_SESSION["Chave2"] = "Valor2";
+
+print_r($_SESSION);
+?>
+
+<?php
+if(!isset($_COOKIE["CDC"])) {
+    setcookie("CDC", "Bala Gelada Recheada", time() + 60, "/", "", "", true); /* SETANDO O COOKIE(NOME, VALOR, TEMPO) */
+    echo "Cookie CDC foi setado!";
+}elseif(isset($_COOKIE["CDC"])){
+    print_r($_COOKIE["CDC"]);
+}
+?>
